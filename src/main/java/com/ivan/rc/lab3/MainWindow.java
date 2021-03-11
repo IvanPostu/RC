5b139a9c33eba3d742eebf15177026868b65fa3c
IvanPostu <ipostu20000127@gmail.com>
2021-03-11 23:18:38 +0200

    #####

--- src/main/java/com/ivan/rc/lab3/MainWindow.java
+++ src/main/java/com/ivan/rc/lab3/MainWindow.java
@@ -1,20 +1,65 @@
-
 package com.ivan.rc.lab3;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.logging.Level;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.methods.RequestBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import sun.misc.IOUtils;
 
 public class MainWindow extends javax.swing.JFrame {
 
+    private static final Logger logger = LogManager.getLogger(MainWindow.class);
+    private Thread optionRequestThread;
+    private Thread headRequestThread;
+
+    static enum RqstType {
+        OPTIONS, HEAD
+    };
 
     public MainWindow() {
         initComponents();
     }
 
     public void setContent(JPanel panel) {
-         contentJPanel.removeAll();
-         contentJPanel.add(panel);
-         contentJPanel.revalidate();
+        contentJPanel.removeAll();
+        contentJPanel.add(panel);
+        contentJPanel.revalidate();
+    }
+
+    @Override
+    public void addNotify() {
+        super.addNotify();
+        optionRequestThread = new Thread(() -> {
+            fetchOptionsRequest();
+        });
+
+        headRequestThread = new Thread(() -> {
+            fetchHeadRequest();
+        });
+
+    }
+
+    @Override
+    public void removeNotify() {
+        try {
+            super.removeNotify();
+            optionRequestThread.join();
+            headRequestThread.join();
+        } catch (InterruptedException ex) {
+            logger.error(ex);
+        }
     }
 
     @SuppressWarnings("unchecked")
@@ -26,6 +71,7 @@ public class MainWindow extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
         contentJPanel = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -55,6 +101,13 @@ public class MainWindow extends javax.swing.JFrame {
             }
         });
 
+        jButton4.setText("Head RQST");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -66,22 +119,25 @@ public class MainWindow extends javax.swing.JFrame {
                 .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 135, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 141, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 191, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 129, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 50, Short.MAX_VALUE)
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 128, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jButton4, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jButton1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jButton2, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel1Layout.createSequentialGroup()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 76, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jLabel1))
-                        .addGap(0, 0, Short.MAX_VALUE))
-                    .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addGap(0, 0, Short.MAX_VALUE)))
                 .addContainerGap())
         );
 
@@ -121,16 +177,120 @@ public class MainWindow extends javax.swing.JFrame {
         this.setContent(new ChatPanel());
     }//GEN-LAST:event_jButton1ActionPerformed
 
+    private void fetchHeadRequest() {
+
+        try (CloseableHttpClient httpclient = HttpClients
+                .custom()
+                .build()) {
+
+            HttpUriRequest request = RequestBuilder.head()
+                    .setUri("http://club.chateg.ru/reg.php")
+                    .build();
+
+            logger.info("Executing request " + request.getRequestLine());
+
+            ResponseHandler<String> responseHandler = response -> {
+                int status = response.getStatusLine().getStatusCode();
+                if (status == 200) {
+                    Header[] headers = response.getAllHeaders();
+                    StringBuilder b = new StringBuilder();
+                    for (Header h : headers) {
+                            b
+                                    .append(h.getName())
+                                    .append(": ")
+                                    .append(h.getValue())
+                                    .append("\n");
+                    }
+
+                    return b.toString();
+                } else {
+                    throw new ClientProtocolException("Unexpected response status: " + status);
+                }
+            };
+
+            String r = httpclient.execute(request, responseHandler);
+            JOptionPane.showMessageDialog(this,
+                    r, "Head request result", JOptionPane.INFORMATION_MESSAGE);
+            logger.info(r);
+        } catch (IOException ex) {
+            logger.error(ex);
+        }
+    }
+
+    private void fetchOptionsRequest() {
+        try (CloseableHttpClient httpclient = HttpClients
+                .custom()
+                .build()) {
+
+            HttpUriRequest request = RequestBuilder.options()
+                    .setUri("https://cat-fact.herokuapp.com")
+                    .build();
+
+            logger.info("Executing request " + request.getRequestLine());
+
+            ResponseHandler<String> responseHandler = response -> {
+                int status = response.getStatusLine().getStatusCode();
+                if (status == 204) {
+                    Header[] headers = response.getAllHeaders();
+                    StringBuilder b = new StringBuilder();
+                    for (Header h : headers) {
+                        if (h.getName().contains("Access-Control")) {
+                            b
+                                    .append(h.getName())
+                                    .append(": ")
+                                    .append(h.getValue())
+                                    .append("\n");
+                        }
+                    }
+
+                    return b.toString();
+                } else {
+                    throw new ClientProtocolException("Unexpected response status: " + status);
+                }
+            };
+
+            String r = httpclient.execute(request, responseHandler);
+            JOptionPane.showMessageDialog(this,
+                    r, "Options request result", JOptionPane.INFORMATION_MESSAGE);
+            logger.info(r);
+        } catch (IOException ex) {
+            logger.error(ex);
+        }
+    }
+
+    private void fetch(RqstType r) {
+        synchronized (this) {
+            if (optionRequestThread.isAlive() || headRequestThread.isAlive()) {
+                return;
+            }
+
+            if (r.equals(RqstType.OPTIONS)) {
+                if (!optionRequestThread.isAlive()) {
+                    optionRequestThread.run();
+                }
+            } else {
+                if (!headRequestThread.isAlive()) {
+                    headRequestThread.run();
+                }
+            }
+        }
+    }
+
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
+        fetch(RqstType.OPTIONS);
     }//GEN-LAST:event_jButton2ActionPerformed
 
- 
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        fetch(RqstType.HEAD);
+    }//GEN-LAST:event_jButton4ActionPerformed
+
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel contentJPanel;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
